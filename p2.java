cd76859144116b6564a6f88316df7b8335240ae4
hemant11-hr <lhrhemant@gmail.com>
2025-01-01 17:31:02 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,9 @@
+public class p2 {
+    public static void main(String[]args){
+        String st1="hemant";
+        String st2="lohar";
+
+        System.out.println("legnth is "+st1.length());
+    }
+    
+}
